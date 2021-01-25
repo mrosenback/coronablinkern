0649789d8ff816ed077e9e65e2fcc7ac1eb1e9ba
mrosenback <mathias.rosenback@ha.ax>
2021-01-25 21:48:29 +0200

    Börjat koda upp en meny för programmet

--- coronablinker.c
+++ coronablinker.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int test;
+    printf("\n---Väkommen Till Corornablinkern---\n\n");
+    printf("1< Mata in öppningskod om du blivit smittad\n2< Mata in identifikationskod\n3< Ta emot ett smittlarm\n\n");
+
+    return 0;
 }
