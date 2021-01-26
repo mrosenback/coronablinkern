cb7aa63e4f387780d89a30e64bcc3ff6d78f6f94
mrosenback <mathias.rosenback@ha.ax>
2021-01-26 14:07:15 +0200

    Lagt till funktioner för de olika menyalternativen och en funktion för nätverkssimulering

--- coronablinker.c
+++ coronablinker.c
@@ -1,8 +1,58 @@
 #include <stdio.h>
+#include <Windows.h>
+
+void networkSimulation() {
+    printf("\nOpening code received. Sending information to the server.\n");
+    
+    for(int i=0;i<3;i++) {
+        Sleep(1000);
+        printf(".");
+    }
+    Sleep(1000);
+    
+    printf("\nInformation has succesfully been sent!");
+}
+
+void openingCode() {
+    int openingCode;
+
+    printf("\n--- Opening Code ---\n\n");
+    printf("Enter your Opening Code: ");
+    scanf("%d", &openingCode);
+    networkSimulation();
+}
+
+void identificationCode() {
+    printf("\n--- Identification Code ---\n\n");
+}
+
+void contagionAlarm() {
+    printf("\n--- Contagion Alarm ---\n\n");
+}
 
 int main() {
-    printf("\n---Väkommen Till Corornablinkern---\n\n");
-    printf("1< Mata in öppningskod om du blivit smittad\n2< Mata in identifikationskod\n3< Ta emot ett smittlarm\n\n");
+    int choice = 5;
 
+    printf("\n--- Welcome to Corornablinkern ---\n\n");
+    printf("1< Enter an Opening Code if you have been infected.\n2< Enter Identification Code.\n3< Receive an Infection Alarm.\n4< Exit.\n\n");
+    
+    while (choice > 4)
+    {
+        printf("Choose category (1, 2 or 3): ");
+        scanf("%d", &choice);
+        
+        if(choice == 1) {
+            openingCode();
+        } else if(choice == 2) {
+            identificationCode();
+        } else if(choice == 3) {
+            contagionAlarm();
+        } else if(choice == 4) {
+            break;
+        } else {
+            printf("Error: Choose a valid category.\n\n");
+        }
+    }
+    
     return 0;
 }
