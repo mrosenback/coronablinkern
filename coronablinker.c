a2a590e88ce6dc38737b660d2db165971066dc37
mrosenback <mathias.rosenback@ha.ax>
2021-01-27 13:31:41 +0200

    Nu funkar funktionen för öppningskod och identifikationskod

--- coronablinker.c
+++ coronablinker.c
@@ -10,7 +10,9 @@ void networkSimulation() {
     }
     Sleep(1000);
     
-    printf("\nInformation has succesfully been sent!");
+    printf("\nInformation has succesfully been sent!\n");
+
+    Sleep(1000);
 }
 
 void openingCode() {
@@ -23,7 +25,46 @@ void openingCode() {
 }
 
 void identificationCode() {
+    int identificationCode;
+    int dd,mm,yy;
+    
     printf("\n--- Identification Code ---\n\n");
+    printf("Enter the Identification Code: ");
+    scanf("%d", &identificationCode);
+
+    while(1) {
+        printf("Enter the date (DD/MM/YYYY): ");
+        scanf("%d/%d/%d", &dd, &mm, &yy);
+        //Check year
+        if (yy >= 1900 && yy <= 9999)
+        {
+            //Check month
+            if (mm >= 1 && mm <= 12)
+            {
+                //Check days
+                if ((dd >= 1 && dd <= 31) && (mm == 1 || mm == 3 || mm == 5 || mm == 7 || mm == 8 || mm == 10 || mm == 12))
+                    break;
+                else if ((dd >= 1 && dd <= 30) && (mm == 4 || mm == 6 || mm == 9 || mm == 11))
+                    break;
+                else if ((dd >= 1 && dd <= 28) && (mm == 2))
+                    break;
+                else if (dd == 29 && mm == 2 && (yy % 400 == 0 || (yy % 4 == 0 && yy % 100 != 0)))
+                    break;
+                else
+                    printf("\nDay is not valid.\n\n");
+            }
+            else
+            {
+                printf("\nMonth is not valid.\n\n");
+            }
+        }
+        else
+        {
+            printf("\nYear is not valid.\n\n");
+        }
+    }
+    
+    networkSimulation();
 }
 
 void contagionAlarm() {
@@ -32,12 +73,11 @@ void contagionAlarm() {
 
 int main() {
     int choice = 5;
-
-    printf("\n--- Welcome to Corornablinkern ---\n\n");
-    printf("1< Enter an Opening Code if you have been infected.\n2< Enter Identification Code.\n3< Receive an Infection Alarm.\n4< Exit.\n\n");
     
-    while (choice > 4)
+    while (1)
     {
+        printf("\n--- Welcome to Coronablinkern ---\n\n");
+        printf("1< Enter an Opening Code if you have been infected.\n2< Enter Identification Code.\n3< Receive an Infection Alarm.\n4< Exit.\n\n");
         printf("Choose category (1, 2 or 3): ");
         scanf("%d", &choice);
         
@@ -48,9 +88,11 @@ int main() {
         } else if(choice == 3) {
             contagionAlarm();
         } else if(choice == 4) {
+            printf("\nExisting program. Goodbye!\n\n");
             break;
         } else {
-            printf("Error: Choose a valid category.\n\n");
+            printf("\nError: Choose a valid category.\n");
+            Sleep(2000);
         }
     }
     
