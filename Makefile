52aa557aa0bac138cac4b8b9ef12bbb773424b5d
mrosenback <mathias.rosenback@ha.ax>
2021-03-03 11:52:48 +0200

    Gjort Makefile för enhetstest

--- Makefile
+++ Makefile
@@ -15,5 +15,15 @@ list.o: list.c list.h
 coronablinker.o: coronablinker.c date.h list.h
 	gcc $(CFLAGS) -c coronablinker.c -o coronablinker.o
 
+datetest.o: datetest.c date.o
+	gcc $(CFLAGS) -c datetest.c -o datetest.o
+
+listtest.o: listtest.c list.o date.o
+	gcc $(CFLAGS) -c listtest.c -o listtest.o
+
 coronablinker: date.o list.o coronablinker.o
 	gcc $(CFLAGS) $(OBJ) -o coronablinker
+
+test: datetest.o listtest.o
+	gcc $(CFLAGS) datetest.o date.o -o datetest
+	gcc $(CFLAGS) listtest.o list.o date.o -o listtest
--- unittest.h
+++ unittest.h
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #ifndef UNITTESTH
 #define UNITTESTH
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_BLUE "\x1b[34m"
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_RESET "\x1b[0m"
 
-#define TEST_CASE(name) printf("\nRUNNING CASE: %s\n", name);
-#define VERIFY(cond, msg) if (cond) printf("SUCCESS: %s\n", msg); else printf("FAIL: %s\n", msg);
+#define TEST_CASE(name) printf(ANSI_COLOR_BLUE"\nRUNNING CASE"ANSI_COLOR_RESET": %s\n", name);
+#define VERIFY(cond, msg) if (cond) printf(ANSI_COLOR_GREEN"SUCCESS"ANSI_COLOR_RESET": %s\n", msg); else printf(ANSI_COLOR_RED"FAIL"ANSI_COLOR_RESET": %s\n", msg);
 
 #endif
